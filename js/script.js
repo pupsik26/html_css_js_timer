036c38b006d2bc578df9015863d977ea06207918
pupsik26 <vadim.kazakov26@yandex.ru>
2021-08-27 00:02:32 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+window.addEventListener('DOMContentLoaded', function() {
+    
+    let allTime = 63;
+    let timer;
+    const seconds = document.getElementById('seconds');
+    const minutes = document.getElementById('minutes');
+    const endTime =  document.getElementById('end');
+
+    countdown(allTime); 
+
+    function countdown() {
+        
+        allTime--;
+        if (allTime < 0) {
+            endTime.innerText = 'Акция закончилась.';
+        } else {
+            let s = allTime % 60;
+            let m = Math.floor(allTime / 60);
+            if (s < 10) seconds.innerText = `0${s}`;
+            else seconds.innerText = s;
+            if (m < 10) minutes.innerText = `0${m}`;
+            else minutes.innerText = m;
+            timer = setTimeout(countdown, 1000);
+        }
+    }
+
+});
